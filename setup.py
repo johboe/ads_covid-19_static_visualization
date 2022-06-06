6a7172c5c1ea6ef98630a2ff97a94e9afeca27a7
Johannes Böhm <johannes.kroev@gmail.com>
2022-06-06 09:55:52 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Static visualization of covid cases for three countries.',
+    author='Johannes',
+    license='',
+)
